ad609425c65c297099f6c2b065611fde44b0dc47
patrickkpowell <patrick@powellboys.com>
2024-01-08 23:57:46 -0600

    Updated README.  Made functions.  General clean up.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # phototag
-AI Photo Tagging
+AI Photo Tagging for Synology photos.  Synology photose uses the XMP:Subject tag to categorize and search photos.  The program reads in any existing XMP:Subject tags from image files, runs AI to generate new tags, concatinates the two lists and writes the XMP field back to the image.
+
+This is currently not ready for use
+
+This code uses huggyface.  To get started with huggyface see this link.
+https://huggingface.co/docs/transformers/installation
--- phototag.py
+++ phototag.py
@@ -6,37 +6,60 @@ import exiftool
 print("Importing huggyface pipeline")
 from transformers import pipeline
 
-print("Importing exif and pillow")
-from exif import Image as ExifImage
-from PIL import Image as PillowImage
-from PIL import ExifTags
-
-img = sys.argv[1]
+# Get any existing Subject EXIF and put into file_data
+def get_existing_subject(image_path):
+  fd = []
+  with exiftool.ExifToolHelper() as et:
+    metadata = et.get_metadata(image_path)
+    for d in metadata:
+      try:
+        #print('d["XMP:Subject"] is: ', type(d["XMP:Subject"]))
+        if not type(d["XMP:Subject"]) is list:
+          #print('d["XMP:Subject"] is a string.  Converting to list.')
+          d["XMP:Subject"] = d["XMP:Subject"].split()
+        for s in d["XMP:Subject"]:
+          #print('s is: ', type(s))
+          fd.append(s)
+      except KeyError:
+        print("No existing XMP:Subject tag in file: Adding XMP:Subject from AI predictions")
+  return fd
 
 # Get AI predictions
-vc = pipeline(model="google/vit-base-patch16-224")
-preds = vc(images=img)
+def get_ai_predictions(image_path):
+  vc = pipeline(model="google/vit-base-patch16-224")
+  preds = vc(images=image_path)
+  # Put AI predictions in pred_data list
+  pd = []
+  #image_path = "with-metadata/{}".format(img)
+  for p in preds:
+    for e in p["label"].split(","):
+      pd.append(e)
+  return pd
 
-# Put AI predictions in pred_data list
-pred_data = []
-#image_path = "with-metadata/{}".format(img)
-for p in preds:
-  for e in p["label"].split(","):
-    pred_data.append(e)
+# Write XMP:Subject to file
+def write_xmp_subject(file_path, subject_data):
+    # Ensure subject_data is a string
+    if not isinstance(subject_data, str):
+        subject_data = str(subject_data)
 
-# Get any existing Subject EXIF and put into file_data
-file_data = []
-with exiftool.ExifToolHelper() as et:
-  metadata = et.get_metadata(img)
-  for d in metadata:
-    try:
-      for s in d["XMP:Subject"]:
-        file_data.append(s)
-    except KeyError:
-      print("No Subject Tag in file: Adding XMP:Subject")
-      # Add Tag here
+    with exiftool.ExifTool() as et:
+        # Set the XMP:Subject tag
+        et.execute(f'-XMP:Subject={subject_data}', file_path)
+
+
+
+img = sys.argv[1]
+# Get existing Subject EXIF and put into file_data
+file_data = get_existing_subject(img)
+# Get AI predictions and put into pred_data
+pred_data = get_ai_predictions(img)
+
 
 # Combine AI and existing tags
-subject = pred_data + file_data
-
+subject = file_data + pred_data
+# Remove leading and trailing whitespace within elements of the list
+subject = [x.strip() for x in subject]
 print('Subject: ', subject)
+
+# Write XMP:Subject to file
+write_xmp_subject(img, subject)
