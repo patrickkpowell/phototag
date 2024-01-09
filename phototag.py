25f52ea321bc7e7651a7810ed5360b6bc2bbe285
patrickkpowell <patrick@powellboys.com>
2024-01-09 17:19:29 -0600

    Moved list processing into list generating functions.  Updated comments.

--- phototag.py
+++ phototag.py
@@ -19,11 +19,23 @@ def get_existing_subject(image_path):
           s = s.replace("'", "")
           s = s.replace("[", "")
           s = s.replace("]", "")
-          print('s value: ', s)
+          # print('s value: ', s)
           fd.append(s)
       except KeyError:
         print("No existing XMP:Subject tag in file: Adding XMP:Subject from AI predictions")
-  print("Existing XMP:Subject tags: ", fd)
+  # Sort and Unique file_data
+  fd = sorted(set(fd))
+  # Remove duplicates
+  fd = list(dict.fromkeys(fd))
+  # Remove empty elements
+  fd = list(filter(None, fd))
+  # Remove elements with only whitespace
+  fd = list(filter(str.strip, fd))
+  # Remove leading and trailing whitespace within elements of the list
+  fd = [x.strip() for x in fd]
+  # Remove ' characters
+  fd = [x.replace("'", "") for x in fd]
+  # print("Existing XMP:Subject tags: ", fd)
   return fd
 
 # Get AI predictions
@@ -36,6 +48,16 @@ def get_ai_predictions(image_path):
   for p in preds:
     for e in p["label"].split(","):
       pd.append(e)
+  # Sort and Unique pred_data
+  pd = sorted(set(pd))
+  # Remove duplicates
+  pd = list(dict.fromkeys(pd))
+  # Remove empty elements
+  pd = list(filter(None, pd))
+  # Remove elements with only whitespace
+  pd = list(filter(str.strip, pd))
+  # Remove leading and trailing whitespace within elements of the list
+  pd = [x.strip() for x in pd]
   return pd
 
 # Write XMP:Subject to file
@@ -44,57 +66,42 @@ def write_xmp_subject(file_path, subject_data):
     if not isinstance(subject_data, str):
         subject_data = str(subject_data)
 
+    # Set the XMP:Subject tag
     with exiftool.ExifTool() as et:
-        # Set the XMP:Subject tag
         et.execute(f'-XMP:Subject={subject_data}', file_path)
 
 
-
+# Process args
+# TODO: Add  handling for args
 img = sys.argv[1]
+
 # Get existing Subject EXIF and put into file_data
 file_data = get_existing_subject(img)
-# Sort and Unique file_data
-file_data = sorted(set(file_data))
-# Remove duplicates
-file_data = list(dict.fromkeys(file_data))
-# Remove empty elements
-file_data = list(filter(None, file_data))
-# Remove elements with only whitespace
-file_data = list(filter(str.strip, file_data))
-# Remove leading and trailing whitespace within elements of the list
-file_data = [x.strip() for x in file_data]
-# Remove ' characters
-file_data = [x.replace("'", "") for x in file_data]
 
 # Get AI predictions and put into pred_data
 pred_data = get_ai_predictions(img)
-# Sort and Unique pred_data
-pred_data = sorted(set(pred_data))
-# Remove duplicates
-pred_data = list(dict.fromkeys(pred_data))
-# Remove empty elements
-pred_data = list(filter(None, pred_data))
-# Remove elements with only whitespace
-pred_data = list(filter(str.strip, pred_data))
-# Remove leading and trailing whitespace within elements of the list
-pred_data = [x.strip() for x in pred_data]
 
+# Build new Subject String
+# Create subject list from file_data
 subject = file_data
+# Set write flag
 write_xmp = False
+# Loop through pred_data and add to subject list if not already there
 for p in pred_data:
   if p not in subject:
     print("New ai tag found: ", p)
     subject.append(p)
+    # Set write flag
     write_xmp = True
+# Convert subject list to string
 strSubject = ', '.join(subject)
-print('Initial Subject String: ', strSubject)
 # Remove list brackets and ' characters
 strSubject = strSubject.replace("'", "")
 strSubject = strSubject.replace("[", "")
 strSubject = strSubject.replace("]", "")
-print('Processed Subject String: ', strSubject)
+# Write new Subject String to file if write flag is set
 if write_xmp:
-  # Write XMP:Subject to file
   write_xmp_subject(img, subject)
+  print("New tags written")
 else:
   print("No new tags to add")
