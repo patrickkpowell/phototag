f688df5293eebe904d3339298a3a8e989d6c0441
patrickkpowell <patrick@powellboys.com>
2024-01-12 00:42:48 -0600

    Output file handling.  Testing different models.

--- phototag.py
+++ phototag.py
@@ -4,14 +4,16 @@ import sys
 import os
 import exiftool
 import torch
+import shutil
 from PIL import Image
 
 print("Importing huggyface pipeline")
 from transformers import pipeline, DetrImageProcessor, DetrForObjectDetection
 
 # Get any existing Subject EXIF and put into file_data
-def get_existing_subject(image_path):
+def get_existing_metadata(image_path):
   fd = []
+  dt = ""
   with exiftool.ExifToolHelper() as et:
     metadata = et.get_metadata(image_path)
     for d in metadata:
@@ -22,10 +24,17 @@ def get_existing_subject(image_path):
           s = s.replace("'", "")
           s = s.replace("[", "")
           s = s.replace("]", "")
-          # print('s value: ', s)
           fd.append(s)
       except KeyError:
         print("No existing XMP:Subject tag in file: Adding XMP:Subject from AI predictions")
+      try:
+        dt = d["EXIF:DateTimeOriginal"].split(" ")[0].replace(":", "-")
+        # Remove leading and trailing whitespace in date_taken
+        dt = dt.strip()
+      except KeyError:
+        print("No EXIF:DateTimeOriginal tag in file")
+        dt = "no_date"
+      print("Date/Time Original: ", dt)
   # Sort and Unique file_data
   fd = sorted(set(fd))
   # Remove duplicates
@@ -39,7 +48,7 @@ def get_existing_subject(image_path):
   # Remove ' characters
   fd = [x.replace("'", "") for x in fd]
   # print("Existing XMP:Subject tags: ", fd)
-  return fd
+  return fd, dt
 
 # Get AI predictions
 def get_ai_predictions(image_path):
@@ -49,23 +58,23 @@ def get_ai_predictions(image_path):
   google_preds = google_vc(images=image_path)#, candidate_labels=['human face','car','gown','Toyota Land Cruiser'])
   models.append(google_preds)
 
-  print("Facebook AI")
-  processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-  model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
-  image = Image.open(img)
-  inputs = processor(images=image, return_tensors="pt")
-  outputs = model(**inputs)
-  # convert outputs (bounding boxes and class logits) to COCO API
-  # let's only keep detections with score > 0.9
-  target_sizes = torch.tensor([image.size[::-1]])
-  results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
-
-  for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-      box = [round(i, 2) for i in box.tolist()]
-      print(
-              f"Detected {model.config.id2label[label.item()]} with confidence "
-              f"{round(score.item(), 3)} at location {box}"
-      )
+  # print("Facebook AI")
+  # processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+  # model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+  # image = Image.open(img)
+  # inputs = processor(images=image, return_tensors="pt")
+  # outputs = model(**inputs)
+  # # convert outputs (bounding boxes and class logits) to COCO API
+  # # let's only keep detections with score > 0.9
+  # target_sizes = torch.tensor([image.size[::-1]])
+  # results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+ 
+  # for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+  #     box = [round(i, 2) for i in box.tolist()]
+  #     print(
+  #             f"Detected {model.config.id2label[label.item()]} with confidence "
+  #             f"{round(score.item(), 3)} at location {box}"
+  #     )
 
   # Put AI predictions in pred_data list
   pd = []
@@ -128,15 +137,26 @@ def write_xmp_subject(fp, sd):
     with exiftool.ExifTool() as et:
         et.execute(f'-XMP:Subject={sd}', fp)
 
+def create_directory(dp):
+    if not os.path.exists(dp):
+        os.makedirs(dp)
+        print(f"Directory created: {dp}")
+    else:
+        print(f"Directory already exists: {dp}")
+
 # Process args
-# TODO: Add  handling for args
-directory = sys.argv[1]
-for filename in os.listdir(directory):
+# TODO:
+#       Add  handling for args
+#       Add handling for directories
+#       Add handling for destination file structure (metatags, dates, etc.)
+src_dir = sys.argv[1]
+#dst_dir = sys.argv[2]
+for filename in os.listdir(src_dir):
   if filename.endswith(".jpg") or filename.endswith(".JPG"):
-    img = os.path.join(directory, filename)
+    img = os.path.join(src_dir, filename)
     print("Processing file: ", img)
     # Get existing Subject EXIF and put into file_data
-    file_data = get_existing_subject(img)
+    file_data, date_taken = get_existing_metadata(img)
     
     # Get AI predictions and put into pred_data
     pred_data = get_ai_predictions(img)
@@ -149,8 +169,24 @@ for filename in os.listdir(directory):
     
     # Write new Subject String to file if write flag is set
     if write_xmp:
-      image_path = "with-metadata/{}".format(img)
-      write_xmp_subject(img, strSubject)
+      if not len(sys.argv) < 2:
+        print("No destination directory specifiedm  Using ", os.getcwd()+"/ai_processed/ as root directory")
+        image_dir = os.getcwd() + "/ai_processed/" + date_taken
+      print(image_dir)
+      create_directory(image_dir)
+      image_path = "{}/{}".format(image_dir, filename)
+      
+      try:
+        shutil.copy(img, image_path)
+      except Exception as e:
+        print(f"Error occurred while copying file: {e}")
+      
+      print("Writing new tags to file: ", image_path)
+      write_xmp_subject(image_path, strSubject)
+      try:
+         os.remove(image_path + '_original')
+      except Exception as e:
+        print(f"Error occurred while deleting copy of original file: {e}")
     else:
       print("No new tags to add")
   else:
